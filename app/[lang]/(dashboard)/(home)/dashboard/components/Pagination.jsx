657a202e64bfa4056bf3188ef9fc0e9e95a4959e
BatistaAlexandre <abatista223@gmail.com>
2024-06-13 10:05:05 -0300

    Some changes (Logo Page view and Candidates

--- app/[lang]/(dashboard)/(home)/dashboard/components/Pagination.jsx
+++ app/[lang]/(dashboard)/(home)/dashboard/components/Pagination.jsx
@@ -1,39 +1,39 @@
-import React from 'react';
+// import React from 'react';
 
-const Pagination = ({ currentPage, totalPages, onPageChange }) => {
-  const handlePreviousPage = () => {
-    if (currentPage > 1) {
-      onPageChange(currentPage - 1);
-    }
-  };
+// const Pagination = ({ currentPage, totalPages, onPageChange }) => {
+//   const handlePreviousPage = () => {
+//     if (currentPage > 1) {
+//       onPageChange(currentPage - 1);
+//     }
+//   };
 
-  const handleNextPage = () => {
-    if (currentPage < totalPages) {
-      onPageChange(currentPage + 1);
-    }
-  };
+//   const handleNextPage = () => {
+//     if (currentPage < totalPages) {
+//       onPageChange(currentPage + 1);
+//     }
+//   };
 
-  return (
-    <div className="flex justify-center items-center mt-4">
-      <button
-        onClick={handlePreviousPage}
-        disabled={currentPage === 1}
-        className="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded-l"
-      >
-        Previous
-      </button>
-      <span className="mx-2">
-        Page {currentPage} of {totalPages}
-      </span>
-      <button
-        onClick={handleNextPage}
-        disabled={currentPage === totalPages}
-        className="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded-r"
-      >
-        Next
-      </button>
-    </div>
-  );
-};
+//   return (
+//     <div className="flex justify-center items-center mt-4">
+//       <button
+//         onClick={handlePreviousPage}
+//         disabled={currentPage === 1}
+//         className="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded-l"
+//       >
+//         Previous
+//       </button>
+//       <span className="mx-2">
+//         Page {currentPage} of {totalPages}
+//       </span>
+//       <button
+//         onClick={handleNextPage}
+//         disabled={currentPage === totalPages}
+//         className="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded-r"
+//       >
+//         Next
+//       </button>
+//     </div>
+//   );
+// };
 
-export default Pagination;
+// export default Pagination;
--- app/[lang]/(dashboard)/(home)/dashboard/page-view.jsx
+++ app/[lang]/(dashboard)/(home)/dashboard/page-view.jsx
@@ -7,9 +7,9 @@ const DashboardPageView = ({ trans }) => {
   return (
     <div className="space-y-6">
       <div className="flex items-center flex-wrap justify-between gap-4">
-        <div className="text-2xl font-medium text-default-800">
+        {/* <div className="text-2xl font-medium text-default-800">
           Analytics {trans?.dashboard}
-        </div>
+        </div> */}
         {/* <DatePickerWithRange /> */}
       </div>
       <Candidates />  {/* Add the Candidates component here */}
